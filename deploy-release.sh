e79d94d7ecf326ae0098409c1aceb9946dc5bda4
Jota Teles <josetelesmaciel@gmail.com>
2017-09-05 09:34:03 -0300

    #1 - Apply small changes to deploy-release.sh

--- deploy-release.sh
+++ deploy-release.sh
@@ -5,13 +5,12 @@ dir=CLONED
 git clone https://github.com/teles/array-mixer.git $dir
 cd $dir
 git checkout master
-sed --in-place '/release\//d' .gitignore || exit 0
-git rm bundle.js
+sed --in-place '/release/d' .gitignore || exit 0
 cat ./.gitignore
 npm install
 npm run release
 git add release
 ls
-git commit -m "Atualizando gh-pages via travis-ci"
+git commit -m "no-issue - updates release files"
 git status
 git push --force "${GH_REFERENCE}" master:master
