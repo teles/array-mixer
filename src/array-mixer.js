0b83d69a0f31306d181e00f58df13710092117c9
Jota Teles <jose.maciel@vivadecora.com.br>
2017-09-06 23:10:54 -0300

    #3 - Refactor array-mixer to remove variables and be cleaner

--- src/array-mixer.js
+++ src/array-mixer.js
@@ -1,41 +1,20 @@
-const translateOrderParam = (order) => {
-
-    let [translatedOrder, pattern] = [[], /([0-9]+)?([a-zA-Z]+)/];
-
-    order.forEach(item => {
-        let matches = item.match(pattern);
-        let count = typeof matches[1] === "undefined" ? 1 : parseInt(matches[1]);
-        let key = matches[2];
-        translatedOrder = translatedOrder.concat(new Array(count).fill(key));
-    });
-    return translatedOrder;
+const getItemsFromArray = (array) => {
+    return (size, offset = 0) => new Array(size + offset).fill("").map((item, index) => {
+        return array[index % array.length];
+    }).slice(offset);
 };
 
-const ArrayMixer = (aliases, order) => {
-    let [arraySize, index, mixedArray] = [0,0, []];
-    let currentAliasName, currentAliasArray, currentAliasIndex, currentAliasCounterValue;
-    let currentAliasesCounters = {};
+const ArrayMixer = (aliases, sequence) => {
+    let [mixedArray, pattern, offsetsByAliases] = [[], /([0-9]+)?([a-zA-Z]+)/, {}];
 
-    order = translateOrderParam(order);
-
-    Object.keys(aliases).forEach(alias => {
-        arraySize+= aliases[alias].length;
+    sequence.forEach(rule => {
+        let matches = rule.match(pattern);
+        let count = typeof matches[1] === "undefined" ? 1 : parseInt(matches[1]);
+        let alias = matches[2];
+        let incrementalOffset = typeof offsetsByAliases[alias] !== "undefined" ? offsetsByAliases[alias] : 0;
+        mixedArray = mixedArray.concat(getItemsFromArray(aliases[alias])(count, offsetsByAliases[alias]));
+        offsetsByAliases[alias] = (count % aliases[alias].length) + incrementalOffset;
     });
-
-    while(index < arraySize){
-        currentAliasName = order[(order.length + index) % order.length];
-        if(typeof currentAliasesCounters[currentAliasName] === "undefined") {
-            currentAliasesCounters[currentAliasName] = 0;
-        }
-        currentAliasArray = aliases[currentAliasName];
-        currentAliasCounterValue = (currentAliasesCounters[currentAliasName]) % currentAliasArray.length;
-        currentAliasesCounters[currentAliasName] = currentAliasCounterValue;
-        currentAliasIndex = currentAliasesCounters[currentAliasName];
-        mixedArray.push(aliases[currentAliasName][currentAliasIndex]);
-        currentAliasesCounters[currentAliasName]++;
-        index++;
-    }
-
     return mixedArray;
 };
 
