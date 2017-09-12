d61dad10e470b87512606f1f0394e63822d775dd
Jota Teles <josetelesmaciel@gmail.com>
2017-09-11 22:12:21 -0300

    no-issue - remove release folder from .gitignore

--- release/array-mixer.es5.js
+++ release/array-mixer.es5.js
@@ -6,9 +6,9 @@ Object.defineProperty(exports, "__esModule", {
 var getItemsFromArray = function getItemsFromArray(array) {
     return function (size) {
         var offset = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : 0;
-        return new Array(size + offset).fill("").map(function (item, index) {
-            return array[index % array.length];
-        }).slice(offset);
+        return new Array(size).fill("").map(function (item, index) {
+            return array[(index + offset) % array.length];
+        });
     };
 };
 
@@ -17,15 +17,27 @@ var ArrayMixer = function ArrayMixer(aliases, sequence) {
         pattern = /([0-9]+)?([a-zA-Z]+)/,
         offsetsByAliases = {};
 
+    var totalAliases = Object.values(aliases).map(function (item) {
+        return item.length;
+    }).reduce(function (total, current) {
+        return total + current;
+    });
 
-    sequence.forEach(function (rule) {
+    var rules = sequence.map(function (rule) {
         var matches = rule.match(pattern);
         var count = typeof matches[1] === "undefined" ? 1 : parseInt(matches[1]);
         var alias = matches[2];
-        var incrementalOffset = typeof offsetsByAliases[alias] !== "undefined" ? offsetsByAliases[alias] : 0;
-        mixedArray = mixedArray.concat(getItemsFromArray(aliases[alias])(count, offsetsByAliases[alias]));
-        offsetsByAliases[alias] = count % aliases[alias].length + incrementalOffset;
+        return { count: count, alias: alias };
     });
+
+    while (mixedArray.length < totalAliases) {
+        rules.forEach(function (rule) {
+            var incrementalOffset = typeof offsetsByAliases[rule.alias] !== "undefined" ? offsetsByAliases[rule.alias] : 0;
+            mixedArray = mixedArray.concat(getItemsFromArray(aliases[rule.alias])(Math.min(totalAliases - mixedArray.length, rule.count), offsetsByAliases[rule.alias]));
+            offsetsByAliases[rule.alias] = rule.count % aliases[rule.alias].length + incrementalOffset;
+            if (mixedArray.length >= totalAliases) return mixedArray;
+        });
+    }
     return mixedArray;
 };
 
